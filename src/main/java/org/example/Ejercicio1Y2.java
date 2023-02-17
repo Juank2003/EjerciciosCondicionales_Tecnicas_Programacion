0757556fd51a09578bf1ba8d7ff43d610748ada5
jkort <jortealv@myuax.com>
2023-02-17 21:23:07 +0100

    Version1

--- src/main/java/org/example/Ejercicio1Y2.java
+++ src/main/java/org/example/Ejercicio1Y2.java
@@ -1,2 +1,17 @@
-package org.example;public class Ejercicio1Y2 {
+package org.example;
+
+//Escriba un algoritmo que muestre "riesgo de hielo", si la temperatura introducida es inferior a 2°C.
+
+import java.util.Scanner;
+
+public class Ejercicio1Y2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce la temperatura: ");
+        int temperatura = sc.nextInt();
+        if (temperatura < 2) {
+            System.out.println("Riesgo de hielo");
+        }
+    }
 }
+
