dad687b188da790842f77c60e3e27baa4b6c2a33
jkort <jortealv@myuax.com>
2023-02-17 21:25:50 +0100

    Version2

--- src/main/java/org/example/Ejercicio1Y2.java
+++ src/main/java/org/example/Ejercicio1Y2.java
@@ -1,5 +1,6 @@
 package org.example;
 
+/*
 //Escriba un algoritmo que muestre "riesgo de hielo", si la temperatura introducida es inferior a 2°C.
 
 import java.util.Scanner;
@@ -11,6 +12,27 @@ public class Ejercicio1Y2 {
         int temperatura = sc.nextInt();
         if (temperatura < 2) {
             System.out.println("Riesgo de hielo");
+        }
+    }
+}*/
+
+//Modificar el algoritmo anterior para mostrar un mensaje según la temperatura:
+
+import java.util.Scanner;
+
+public class Ejercicio1Y2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce la temperatura: ");
+        int temperatura = sc.nextInt();
+        if (temperatura < 2) {
+            System.out.println("Riesgo de hielo");
+        } else if (temperatura >= 2 && temperatura < 15) {
+            System.out.println("Hace frío");
+        } else if (temperatura >= 15 && temperatura < 30) {
+            System.out.println("Buena temperatura");
+        } else {
+            System.out.println("Demasiado calor");
         }
     }
 }
